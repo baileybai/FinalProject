0c23d5e023ca6932646dea0b8f6ea16d9fc11205
reatured <lz2729@tc.columbia.edu>
2020-04-26 16:44:11 -0400

    Add the doing window

---
+++ app/src/main/java/com/example/android/recycylerview2020/FinishedTaskList.java
@@ -0,0 +1,14 @@
+package com.example.android.recycylerview2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishedTaskList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finished_task_list);
+    }
+}
